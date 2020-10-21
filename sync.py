44f4b64026208f4bfc5a1f5deda39d41267ce7e5
Jelle Schutter <jelle@schutter.xyz>
2020-10-21 12:13:17 +0200

    change api client package

--- sync.py
+++ sync.py
@@ -1,15 +1,9 @@
 # import the library
-import data_import as weather
+import fhnw_ds_weatherstation_client as weather
 import os
 
 # DB and CSV config
 config = weather.Config()
-# define CSV path (you need to define this based on your environment)
-config.historic_data_folder='.'+os.sep+'data'
-# set batch size for DB inserts (decrease for raspberry pi)
-config.historic_data_chunksize=10000
-# define DB host
-config.db_host='localhost'
 
 # connect to DB
 weather.connect_db(config)
