46aac2180112c6b2893ee635ed58a45c843b7d3a
Pascal Berger <phyti1phyti1@gmail.com>
2020-10-22 15:17:22 +0200

    added autostart script

---
+++ autostart.sh
@@ -0,0 +1,7 @@
+#python3.8 app.py
+
+@xscreensaver -no-splash  # comment this line out to disable screensaver
+@xset s off
+@xset -dpms
+@xset s noblank
+@chromium-browser --incognito --kiosk http://localhost:8050/ 
