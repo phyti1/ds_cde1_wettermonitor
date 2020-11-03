1fd174e062e01772feb62a8126f926c509f5db5a
Pascal Berger <phyti1phyti1@gmail.com>
2020-11-03 23:22:20 +0100

    added delay to startup file to allow system to load first

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,13 @@
 #python3.8 app.py
 
-@xscreensaver -no-splash  # comment this line out to disable screensaver
-@xset s off
-@xset -dpms
-@xset s noblank
-@chromium-browser --incognito --kiosk http://localhost:8050/ 
+#@xscreensaver -no-splash  # comment this line out to disable screensaver
+#@xset s off
+#@xset -dpms
+#@xset s noblank
+#chromium-browser --incognito --kiosk http://localhost:8050/
+
+#wait for os to fully load
+sleep 10s
+
+python3 /home/pi/wettermonitor/app.py &
+
