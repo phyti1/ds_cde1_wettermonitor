4f6bee676cd423bdc9ab9ae4a7a23add026e8fb7
Pascal Berger <phyti1phyti1@gmail.com>
2020-11-17 08:10:39 +0100

    minor changes chrome autostart

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@
 import sys
 import time
 import threading
+import sys
 
 from selenium import webdriver
 from selenium.webdriver.support import ui
@@ -49,16 +50,20 @@ class Main:
         option.add_argument("--remote-debugging-port=9222")
         option.add_argument("disable-extensions")
         # find binary with "which chromium" or "which chromium-browser" in bash
-        option.binary_location = "/usr/bin/chromium-browser"
+        option.binary_location = "/snap/bin/chromium"
         # overcome problem python executed as root
         option.add_argument("--disable-dev-shm-usage")
+        option.add_experimental_option("excludeSwitches", ['enable-automation'])
         # option.add_argument('--headless')
 
-        self.driver = webdriver.Chrome(options=option)
+        self.driver = webdriver.Chrome("./assets/chromedriver", options=option)
         threading.Thread(target=self.refresh_page).start()
 
     def run(self):
-        self.open_chromium()
+        self.frontend.run()
+        #TODO test
+        if(sys.platform == "Ubuntu 20.04"):
+            self.open_chromium()
         try:
             self.frontend.app.run_server(debug=False)
         except AttributeError as err:
