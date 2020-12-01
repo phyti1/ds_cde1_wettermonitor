83ed146c6c3e3185b56463de8fc334f72646aa5c
Pascal Berger <phyti1phyti1@gmail.com>
2020-12-01 17:19:29 +0100

    main method comments

--- Main.py
+++ Main.py
@@ -15,12 +15,19 @@ from lib.Frontend import Frontend
 class Main:
 
     def __init__(self):
+        """ (void) -> void
+        Constructer of the Main class.
+        Will initialize dash and frontend classes.
+        """
         # initialize Dash
         app = dash.Dash(__name__, title = 'Wettermonitor')
         # instanciate Frontend
         self.frontend = Frontend(app)
 
     def run(self):
+        """ (void) -> void
+        Function to run the software.
+        """
         # load user interface
         self.frontend.run()
         # start Dash server
